dcb50c33d62c80abd190234920f7f404e0b9ec71
Martin Nestorov <mnestorov@protonmail.com>
2020-04-30 22:28:34 +0300

    Add ch4, task 2 and 3 solutions

--- clj-brave-true/src/clj_brave_true/ch4.clj
+++ clj-brave-true/src/clj_brave_true/ch4.clj
@@ -9,6 +9,13 @@
 
 (def vamp-keys [:name :glitter-index])
 
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
 (defn convert
   [vamp-key value]
   ((get conversions vamp-key) value))
@@ -26,7 +33,41 @@
   "Return a list of names that pass the glitter-filter"
   [minimum-glitter records]
   (map #(:name %)
-   (filter (fn [vamp]
-            (if (>= (:glitter-index vamp) minimum-glitter)
-              (:name vamp)))
-           records)))
+       (filter (fn [vamp]
+                 (if (>= (:glitter-index vamp) minimum-glitter)
+                   (:name vamp)))
+               records)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Task 2
+;;;;;;;;;;;;;;;;;;;;
+
+(defn append-suspect
+  "Appends a suspect to the list"
+  [name glitter-index]
+  (conj names [(str name) (str glitter-index)]))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Task 3
+;;;;;;;;;;;;;;;;;;;;
+
+(defn has-name?
+  [n]
+  (true? n))
+
+(defn has-glitter?
+  [n]
+  (> n -1))
+
+(def valid-suspect {:name has-name?
+                    :glitter-index has-glitter?})
+
+(defn is-valid-suspect?
+  [m k v]
+  ((get m k) v))
+
+(defn validate
+  [keywords record]
+  (if (and
+       (is-valid-suspect? valid-suspect :name (:name record))
+       (is-valid-suspect? valid-suspect :glitter-index (:glitter-index record)))))
